86ac793f1497be7764bb8e2ec8f11f739bbc6748
VictorHAS <victorhermesalves@gmail.com>
2020-05-23 19:40:33 -0300

    improvements: adds - on type outcome

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -106,7 +106,9 @@ const Dashboard: React.FC = () => {
                 <tr key={transaction.id}>
                   <td className="title">{transaction.title}</td>
                   <td className={transaction.type}>
-                    {transaction.formattedValue}
+                    {`${transaction.type === 'outcome' ? '- ' : ''}${
+                      transaction.formattedValue
+                    }`}
                   </td>
                   <td>{transaction.category.title}</td>
                   <td>{transaction.formattedDate}</td>
